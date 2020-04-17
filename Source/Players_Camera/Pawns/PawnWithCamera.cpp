82e953da4b81748d0b96201ea70aa9feb7540b96
JunS <jmob0706unpc@live.jp>
2020-04-17 16:17:57 +0900

    [add] 使うポーンの作成

---
+++ Source/Players_Camera/Pawns/PawnWithCamera.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnWithCamera.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APawnWithCamera::APawnWithCamera()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// ルートコンポーネント
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	// バネコンポーネント
+	OurCameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSprintArm"));
+	OurCameraSpringArm->SetupAttachment(RootComponent);
+	// 初期位置
+	OurCameraSpringArm->SetRelativeLocationAndRotation(FVector(0.f, 0.f, 50.f), FRotator(-60.f, 0.f, 0.f));
+	// 長さ
+	OurCameraSpringArm->TargetArmLength = 400.f;
+	// カメラ用設定
+	OurCameraSpringArm->bEnableCameraLag = true;
+	OurCameraSpringArm->CameraLagSpeed = 3.f;
+
+	// カメラコンポーネント
+	OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("GameCamera"));
+	// バネにアタッチ
+	OurCamera->SetupAttachment(OurCameraSpringArm);
+
+	// プレイヤー設定
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void APawnWithCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APawnWithCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APawnWithCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Players_Camera/Pawns/PawnWithCamera.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PawnWithCamera.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class PLAYERS_CAMERA_API APawnWithCamera : public APawn
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* OurCameraSpringArm;
+
+	UCameraComponent* OurCamera;
+
+public:
+	// Sets default values for this pawn's properties
+	APawnWithCamera();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
